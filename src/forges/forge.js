9c92aaf73e1b0b3016e16bc65309a2a816fa1eff
Rafael Mardojai CM <email@rafaelmardojai.com>
2023-04-30 12:47:44 -0500

    forges: Improve Forge class documentation

--- src/forges/forge.js
+++ src/forges/forge.js
@@ -12,12 +12,12 @@ export default class Forge {
     static name = 'github';
 
     /**
-     * Name for display of the forge
+     * Name of the forge for display
      */
     static prettyName = 'Github';
 
     /**
-     * If the forge allow setting an instance url
+     * If the forge allows setting an instance url
      */
     static allowInstances = false;
 
@@ -33,16 +33,35 @@ export default class Forge {
 
     /**
      * Crete a Forge
+     * 
      * @param {String} url The url of the forge
      * @param {String} token The access token
      * @param {String} account Account ID associated to the instance
      * @param {String} accountName Account name associated to the instance
      */
     constructor(url, token, account=null, accountName='') {
+        /**
+         * URL passed when the class was instantiated, the same as
+         * this.defaultURL if this.allowInstances is false
+         */
         this.url = url;
+
+        /**
+         * Account access token passed when the class was instantiated, used for
+         * authentication
+         */
         this.token = token;
+
+        /**
+         * Account ID on Forge Sparks settings
+         */
         this.account = account;
+
+        /**
+         * Account name (username@instance.tld)
+         */
         this.accountName = accountName;
+        
         this.modifiedSince = '';
         this.encoder = new TextEncoder();
         this.decoder = new TextDecoder('utf-8');
@@ -50,6 +69,13 @@ export default class Forge {
 
     /**
      * Get the username
+     * 
+     * Get username from the logged in user.
+     * Used for validating an access token when adding a new account.
+     * 
+     * @throws {FailedForgeAuth} The access token is not valid (401 status)
+     * @throws {Unexpected} Got a response but could not find the username
+     * @throws Any other error when making the request or reading the response
      * @returns {String} The username
      */
     async getUser() {
@@ -58,6 +84,10 @@ export default class Forge {
 
     /**
      * Get notifications
+     * 
+     * Get all unread notifications for the logged in user
+     * 
+     * @throws Will throw an error if some part of the process fails
      * @returns {Array<Notification>} The notifications
      */
     async getNotifications() {
@@ -66,8 +96,10 @@ export default class Forge {
 
     /**
      * Mark notifications as read
+     * 
      * @param {String|Number|null} id ID of the notification to mark as read
      * or null if all should be marked.
+     * @throws Will throw an error if some part of the process fails
      * @returns {Boolean} If the operation was successful
      */
     async markAsRead(id=null) {
@@ -76,6 +108,11 @@ export default class Forge {
 
     /**
      * Helper for creating Soup.Message
+     * 
+     * Coverts data to JSON string and encodes it to bytes
+     * Adds access token as Authorization http header
+     * Adds UTC as Time-Zone http header
+     * 
      * @param {String} method HTTP method for the message
      * @param {String} url URL for the message
      * @param {Object} headers HTTP headers for the message
@@ -105,7 +142,11 @@ export default class Forge {
 
     /**
      * Read the contents of a response
+     * 
+     * Converts bytes to a JS Object
+     * 
      * @param {GLib.Bytes} bytes Bites to read content from
+     * @returns {Object}
      */
     readContents(bytes) {
         const contents = this.decoder.decode(bytes.get_data());
@@ -119,6 +160,7 @@ export default class Forge {
 
     /**
      * Create a more unique ID using the forge account ID
+     * 
      * @param {String|Number} id ID to make unique
      * @returns {String}
      */
@@ -128,6 +170,7 @@ export default class Forge {
 
     /**
      * Build a request URI from multiple parts
+     * 
      * @param {String} host The URI host
      * @param {String} path The URI path
      * @param {Object.<string, string>} query The URI query
@@ -135,15 +178,18 @@ export default class Forge {
      */
     static buildURI(host, path, query={}) {
 
+        /* Prepend slash to the path if not present */
         if (!path.startsWith('/')) {
             path = '/' + path;
         }
 
+        /* Generate query string from the query dictionary */
         var queryString = Object.keys(query).map(key => key + '=' + query[key]).join('&').replace(' ', '+');
         if (!queryString) {
             queryString = null
         }
 
+        /* Build a https URI with the elements */
         const uri = GLib.Uri.build(
             GLib.UriFlags.PARSE_RELAXED,
             'https',
