02160f90bc309374a97b89714e1db539948efb2c
Rafael Mardojai CM <email@rafaelmardojai.com>
2023-05-01 00:31:07 -0500

    preferences: Improve code documentation

--- src/preferences.js
+++ src/preferences.js
@@ -34,7 +34,7 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
         super(constructProperties);
 
         this.forges = Object.values(FORGES);
-        this._editing = null; // Account begin edited
+        this._editing = null; // AccountObject begin edited
 
         /* Bind accounts list */
         this._accountsList.bind_model(accounts, this._createAccountRow.bind(this));
@@ -63,36 +63,80 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
         this._forge.model = forgesList
     }
 
+    /**
+     * Callback for when the keep running on background pref (hide-on-close) is
+     * toggled (this._background::notify::enable-expansion)
+     */
     async _onBackgroundChanged() {
+        /* If saved pref have not changed, return */
         if (this._background.enable_expansion == settings.get_boolean('hide-on-close'))
             return;
 
+        /* Check if autostart should also be requested */
         const autostart = (settings.get_boolean('autostart') && this._background.enable_expansion);
+        /* Request background permission and possibly autostart to the portal */
         const success = await requestBackground(this, autostart);
-        settings.set_boolean('hide-on-close', (this._background.enable_expansion && success));
+        /* New boolean value depends on success and user choice */
+        const newValue = (this._background.enable_expansion && success)
 
+        /* Update the saved the preference */
+        settings.set_boolean('hide-on-close', newValue);
+        /* Update the app window hide_on_close prop with the new value */
         const app = Gtk.Application.get_default();
-        app.window.hide_on_close = this._background.enable_expansion;
+        app.window.hide_on_close = newValue;
+
+        /* If new value differs from user choice, force user choice to result */
+        if (this._background.enable_expansion != newValue)
+            this._background.enable_expansion = newValue;
+
+        /* If success was false, show a toast */
+        if (!success)
+            this.add_toast(new Adw.Toast({
+                title: _('The request failed.')
+            }));
     }
 
+    /**
+     * Callback for when the run on startup pref (autostart) is toggled
+     * (this._startup::notify::active)
+     */
     async _onStartupChanged() {
+        /* If saved pref have not changed, return */
         if (this._startup.active == settings.get_boolean('autostart'))
             return;
 
+        /* Request background permission and autostart new value to the portal */
         const success = await requestBackground(this, this._startup.active);
-        const result = (this._startup.active && success)
+        /* New boolean value depends on success and user choice */
+        const newValue = (this._startup.active && success)
 
-        settings.set_boolean('autostart', result);
+        /* Update the saved the preference */
+        settings.set_boolean('autostart', newValue);
 
+        /* Update the widget back with the actual new value */
         this._startup.freeze_notify();
-        this._startup.active = result;
+        this._startup.active = newValue;
         this._startup.thaw_notify();
+
+        /* If success was false, show a toast */
+        if (!success)
+            this.add_toast(new Adw.Toast({
+                title: _('The autostart request failed.')
+            }));
     }
 
+    /**
+     * Present the new account view
+     */
     _onOpenAddAccount() {
         this.present_subpage(this._accountNew);
     }
 
+    /**
+     * Present the edit account view
+     * 
+     * @param {AccountObject} account The account to edit
+     */
     async _onEditAccount(account) {
         this._accountEditTitle.subtitle = account.displayName;
 
@@ -110,10 +154,16 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
         this._accessTokenEditHelp.label = FORGES[account.forge].tokenText
 
         this._editing = account // Set account begin edited
-        this._editing.token = token;
+        this._editing.token = token; // Save current account token
+
         this.present_subpage(this._accountEdit);
     }
 
+    /**
+     * Go back to main preferences view
+     * 
+     * Also reset fields from edit and create account views 
+     */
     _onBack() {
         this.close_subpage();
 
@@ -129,14 +179,30 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
         this._editing = null; // Reset any account begin edited
     }
 
+    /**
+     * Get selected forge name from the new account view 
+     * 
+     * @returns {String} Fhe forge name
+     */
     _getSeletedForge() {
         return this.forges[this._forge.selected].name
     }
 
+    /**
+     * Get if selected forge in new account view allows instances
+     * 
+     * @returns {Boolean} If it allows instances
+     */
     _allowInstances() {
         return this.forges[this._forge.selected].allowInstances;
     }
 
+    /**
+     * Get instance url set in the new account view.
+     * Or forge default url if it doesn't allow instances
+     * 
+     * @returns {String} If it allows instances
+     */
     _getInstanceURL() {
         if (!this._allowInstances()) {
             return this.forges[this._forge.selected].defaultURL;
@@ -149,6 +215,7 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
 
     /**
      * Get host from GLib.Uri with the www removed
+     * 
      * @param {GLib.Uri} uri URL to get the host
      * @returns {String} The URI host
      */
@@ -160,6 +227,9 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
         return host;
     }
 
+    /**
+     * Callback for when the selected forge changes in the add new account view
+     */
     _onForgeChanged() {
         /* Enable or disable instance URL entry */
         this._instance.sensitive = this._allowInstances();
@@ -168,8 +238,10 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
     }
 
     /**
-     * Validate and get GLib.Uri
+     * Validate and get GLib.Uri from url string
+     * 
      * @param {String} url URL to validate
+     * @trows Trows an error if GLib failed parsing the url
      * @returns {GLib.Uri} Parser URI
      */
     _validateUrl(url) {
@@ -186,6 +258,12 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
         }
     }
 
+    /**
+     * Callback for when any entry changes in the add new account view.
+     * Instance url and access token.
+     * 
+     * Updates add account button sensitivity after validating the values
+     */
     _onEntryChanged() {
         let valid = false;
 
@@ -196,6 +274,9 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
                 valid = this._accessToken.text != '' && this._instance.text != '';
             } catch (error) {
                 this._instance.add_css_class('error');
+                this.add_toast(new Adw.Toast({
+                    title: _("Invalid instance url.")
+                }));
             }
         } else {
             valid = this._accessToken.text != '';
@@ -204,8 +285,14 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
         this._addAccountBtn.sensitive = valid;
     }
 
+    /**
+     * Callback for when any entry changes in the edit account view.
+     * Instance url and access token.
+     * 
+     * Updates save account button sensitivity after validating the values and
+     * checking that at last one has changed.
+     */
     _onEditEntryChanged() {
-        /* Enable or disable Save account button */
         if (this._editing != null) {
             let valid = false;
             const instances = FORGES[this._editing.forge].allowInstances;
@@ -224,6 +311,9 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
                     );
                 } catch (error) {
                     this._instanceEdit.add_css_class('error');
+                    this.add_toast(new Adw.Toast({
+                        title: _("Invalid instance url.")
+                    }));
                 }
             } else {
                 valid = tokenNotEmpty && tokenChanged;
@@ -233,6 +323,11 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
         }
     }
 
+    /**
+     * Get error user visible text
+     * 
+     * @returns {String} The error text
+     */
     _errorText(error) {
         switch (error) {
             case 'FailedForgeAuth':
@@ -242,16 +337,25 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
         }
     }
 
+    /**
+     * Callback when the user adds an account.
+     * Save account in settings.
+     */
     async _onAddAccount() {
         try {
+            /* Make the whole form insensitive */
             this._accountNew.sensitive = false;
 
+            /* Get form values */
             const token = this._accessToken.text;
             const url = this._getInstanceURL();
             const forgeName = this._getSeletedForge();
-            const forge = new FORGES[forgeName](url, token);
-            const username = await forge.getUser();
 
+            /* Instantiate the class for the forge */
+            const forge = new FORGES[forgeName](url, token);
+            /* Try authenticating the user with access token */
+            const username = await forge.getUser();
+            /* Save account to settings */
             await accounts.saveAccount(
                 forgeName,
                 url,
@@ -259,21 +363,29 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
                 token
             );
 
-            const toast = new Adw.Toast({ title: _("New account added successfully!") });
-            this.add_toast(toast);
+            this.add_toast(new Adw.Toast({
+                title: _("New account added successfully!")
+            }));
             this._onBack();
         } catch (error) {
             console.log(error);
-            const errorToast = new Adw.Toast({ title: this._errorText(error) });
-            this.add_toast(errorToast);
+            this.add_toast(new Adw.Toast({
+                title: this._errorText(error)
+            }));
         } finally {
             this._accountNew.sensitive = true;
         }
     }
 
+    /**
+     * Callback when the user saves an account new preferences.
+     * Update account in settings.
+     */
     async _onUpdateAccount() {
+        /* Make the whole form insensitive */
         this._accountEdit.sensitive = false;
 
+        /* Get and validate from values */
         const forgeClass = FORGES[this._editing.forge];
         let newToken = this._accessTokenEdit.text;
         let newUrl = this._instanceEdit.text;
@@ -284,6 +396,7 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
             newUrl = this._getUriHost(newUrl);
         }
 
+        /* Continue if some value has actually changed */
         if (newToken != this._editing.token || newUrl != this._editing.url) {
             try {
                 const forge = new forgeClass(newUrl, newToken);
@@ -296,18 +409,24 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
                     newToken
                 );
 
-                const toast = new Adw.Toast({ title: _("Account edited successfully!") });
-                this.add_toast(toast);
+                this.add_toast(new Adw.Toast({
+                    title: _("Account edited successfully!")
+                }));
                 this._onBack();
             } catch (error) {
                 console.log(error);
-                const errorToast = new Adw.Toast({ title: this._errorText(error) });
-                this.add_toast(errorToast);
+                this.add_toast(new Adw.Toast({
+                    title: this._errorText(error)
+                }));
             }
         }
+
         this._accountEdit.sensitive = true;
     }
 
+    /**
+     * Callback when the user removes an account
+     */
     async _onRemoveAccount() {
         const errorToast = new Adw.Toast({ title: _("Unexpected error removing the account") });
         try {
@@ -322,6 +441,12 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
         this._onBack();
     }
 
+    /**
+     * Create a widget for each saved account
+     * 
+     * @param {AccountObject} account The account object
+     * @returns {Gtk.Widget} The widget representing the account
+     */
     _createAccountRow(account) {
         const row = new Adw.ActionRow({
             title: account.displayName,
@@ -338,5 +463,4 @@ export default class PreferencesWindow extends Adw.PreferencesWindow {
 
         return row;
     }
-
 };
